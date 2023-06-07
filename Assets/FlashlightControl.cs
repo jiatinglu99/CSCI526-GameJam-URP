95aebc734d79233600894940f70e19a3d07cd701
Terry Lu <terrylu@usc.edu>
2023-06-07 03:44:34 -0700

    add maze terrain and basic player control

---
+++ Assets/FlashlightControl.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashlightControl : MonoBehaviour
+{
+    private GameObject flashLight;
+    private GameObject cheatLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        flashLight = GameObject.Find("/Player/Flashlight");
+        cheatLight = GameObject.Find("/Player/CheatLight");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if space is pressed, toggle the flashlight
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            flashLight.SetActive(!flashLight.activeSelf);
+        }
+        // if c is pressed, toggle the cheat light
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            cheatLight.SetActive(!cheatLight.activeSelf);
+        }
+    }
+}
---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float speed = 10.0f;
+    [SerializeField] private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical   = Input.GetAxis("Vertical");
+
+        Vector3 movement;
+        if (moveHorizontal != 0)
+        {
+            movement = new Vector3(moveHorizontal, 0, 0);
+        }
+        else
+        {
+            movement = new Vector3(0, 0, moveVertical);
+        }
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        // rotate the player to face the moving direction
+        if (movement != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(movement);
+            transform.Translate(-movement, Space.World);
+        }
+
+        // Debug.Log(rb.velocity);
+    }
+}
