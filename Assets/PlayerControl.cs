33576ff7d76b6d0c3e3a0d5846f4a669a3a9dbf7
Terry Lu <terrylu@usc.edu>
2023-06-07 20:29:44 -0700

    adding flashlight and cheatlight control

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    public float speed = 10.0f;
-    [SerializeField] private Rigidbody rb;
+    public float speed = 100.0f;
+    public float turnSpeed = 20.0f;
+    private Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -28,13 +29,17 @@ public class PlayerControl : MonoBehaviour
         {
             movement = new Vector3(0, 0, moveVertical);
         }
-        movement = movement.normalized * speed * Time.deltaTime;
+        movement = movement.normalized * speed * Time.fixedDeltaTime;
 
         // rotate the player to face the moving direction
         if (movement != Vector3.zero)
         {
-            transform.rotation = Quaternion.LookRotation(movement);
-            transform.Translate(-movement, Space.World);
+            // transform.rotation = Quaternion.LookRotation(movement);
+            // transform.Translate(-movement, Space.World);
+            Quaternion toRotation = Quaternion.LookRotation(movement);
+            transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, turnSpeed * Time.deltaTime);
+            // transform.Translate(-movement, Space.World);
+            rb.MovePosition(transform.position - movement);
         }
 
         // Debug.Log(rb.velocity);
