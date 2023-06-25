e218144354a823408031b1e7438b2470cae997c3
Megan Friedenberg <mfrieden@usc.edu>
2023-06-24 17:53:41 -0700

    Successfully linked to Firebase, Analytics.cs does the POST request, merging to main

--- Assets/Analytics.cs
+++ Assets/Analytics.cs
@@ -3,57 +3,65 @@ using System.Collections.Generic;
 using UnityEngine.Networking;
 using System.Collections;
 using System;
+using Proyecto26;
 
 [System.Serializable]
 public class PlayerData
 {
-    public string name;
-    public int score;
-
+    public int userID;
+    public int highestCompletedLevel;
+    public float[] timeSpent = new float[2]; // change 2 based on how many levels there are
+    public int[] timesRetried = new int[2]; // ^^^
 }
 
 public class Analytics : MonoBehaviour
 {
     private IEnumerator Start()
     {
-
+        System.Random random = new System.Random();
         Debug.Log("OVER HERE");
         PlayerData playerData = new PlayerData();
-        playerData.name = "John";
-        playerData.score = 100;
+        playerData.userID = random.Next(1, 101); // generates between 1 and 100
+        playerData.highestCompletedLevel = 0;
+        playerData.timeSpent[0] = 0.0f;
+        playerData.timesRetried[0] = 0;
 
         string json = JsonUtility.ToJson(playerData);
 
-        string URL = "https://jomandterry-3569d.firebaseio.com";
-        string key = "/example2";
+        RestClient.Post("https://jomandterry-3569d-default-rtdb.firebaseio.com/.json", playerData);
+        yield break;
+        //string URL = "https://jomandterry-3569d.firebaseio.com";
+        //string key = "/example2";
 
-        using (var uwr = new UnityWebRequest(URL + key + ".json", "POST"))
-        {
+        //using (var uwr = new UnityWebRequest(URL + key + ".json", "POST"))
+        //{
 
-            byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(json);
-            using UploadHandlerRaw uploadHandler = new UploadHandlerRaw(jsonToSend);
-            uwr.uploadHandler = uploadHandler;
-            uwr.downloadHandler = new DownloadHandlerBuffer();
-            uwr.disposeUploadHandlerOnDispose = true;
-            uwr.disposeDownloadHandlerOnDispose = true;
-            uwr.SetRequestHeader("Content-Type", "application");
-            uwr.timeout = 5;
-            // Send the request then wait here till it returns
-            yield return uwr.SendWebRequest();
-            Debug.Log("HERE!");
+        //    byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(json);
+        //    using UploadHandlerRaw uploadHandler = new UploadHandlerRaw(jsonToSend);
+        //    uwr.uploadHandler = uploadHandler;
+        //    uwr.downloadHandler = new DownloadHandlerBuffer();
+        //    uwr.disposeUploadHandlerOnDispose = true;
+        //    uwr.disposeDownloadHandlerOnDispose = true;
+        //    uwr.SetRequestHeader("Content-Type", "application");
+        //    uwr.timeout = 5;
+        //    // Send the request then wait here till it returns
+        //    yield return uwr.SendWebRequest();
+        //    Debug.Log("HERE!");
 
-            //string timestamp = GlobalAnalysis.getTimeStamp();
+        //    //string timestamp = GlobalAnalysis.getTimeStamp();
 
-            if (uwr.result != UnityWebRequest.Result.Success)
-            {
-                Debug.Log("Error While Sending: " + uwr.error);
-            }
-            else
-            {
-                Debug.Log("Data Received: " + uwr.downloadHandler.text);
-            }
+        //    if (uwr.result != UnityWebRequest.Result.Success)
+        //    {
+        //        Debug.Log("Error While Sending: " + uwr.error);
+        //    }
+        //    else
+        //    {
+        //        Debug.Log("Data Received: " + uwr.downloadHandler.text);
+        //    }
 
-        }
+        //}
+
+
         //UnityWebRequest request = UnityWebRequest.Post(databaseURL + endpoint, json);
         //request.SetRequestHeader("Content-Type", "application");
 
