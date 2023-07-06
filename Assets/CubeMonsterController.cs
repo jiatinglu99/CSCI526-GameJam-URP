c6090c261b0d3e617539235f48120bc39df3b467
Jason Chen <jsn.chn@outlook.com>
2023-07-06 12:38:13 -0700

    Implement torque instead of axis/anchor

--- Assets/CubeMonsterController.cs
+++ Assets/CubeMonsterController.cs
@@ -17,14 +17,22 @@ public class CubeMonsterController : MonoBehaviour
     }
 
     // Update is called once per frame
+    // void Update()
+    // {
+    //     anchor = transform.position + new Vector3(0,-1,1) * 20.0f;
+    //     axis = Vector3.Cross(Vector3.up, new Vector3(0,0,1));
+    //     if (rolling == true) {
+    //         for (int i = 0; i < (90 / rollSpeed); i++) {
+    //             transform.RotateAround(anchor, axis, rollSpeed);
+    //         }
+    //     }
+    // }
+
     void Update()
     {
-        anchor = transform.position + new Vector3(0,-1,1) * 20.0f;
-        axis = Vector3.Cross(Vector3.up, new Vector3(0,0,1));
-        if (rolling == true) {
-            for (int i = 0; i < (90 / rollSpeed); i++) {
-                transform.RotateAround(anchor, axis, rollSpeed);
-            }
+        if (rolling) {
+            Vector3 torque = new Vector3(0, 0, rollSpeed);
+            rb.AddTorque(torque, ForceMode.VelocityChange);
         }
     }
 }
